848ea5f35eaf2384c38c609e87ba1758a9bc9da9
ApoorvBrooklyn <apoorobot21@gmail.com>
2024-07-26 18:21:25 +0530

    Removing All the versions

--- src/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from question_answering import answer_question
-
-st.title("Textbook Question Answering System")
-
-query = st.text_input("Enter your question:")
-if query:
-    answer = answer_question(query)
-    st.write("Answer:", answer)
